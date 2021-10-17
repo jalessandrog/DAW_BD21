bf8c52a4ac538968cebcbfdd84113403525be44b
Joseph Alessandro García García <a01701434@itesm.mx>
2021-10-17 16:49:44 -0500

    requisito de autenticación para rutas de modificación de información de heroe

--- Laboratorios/Lab23/routes/heroes.js
+++ Laboratorios/Lab23/routes/heroes.js
@@ -7,8 +7,8 @@ const heroes_controller = require('../controllers/heroes_controller')
 router.get('/add', isAuth, heroes_controller.addHeroe)
 router.post('/add', isAuth, heroes_controller.saveHeroe)
 
-router.get('/editar/:heroe_id', heroes_controller.updateHeroe);
-router.post('/editar/:heroe_id', heroes_controller.processUpdate);
+router.get('/editar/:heroe_id', isAuth, heroes_controller.updateHeroe);
+router.post('/editar/:heroe_id', isAuth, heroes_controller.processUpdate);
 
 router.get('/detalle/:heroe_id', heroes_controller.seeHeroe);
 router.get('/', heroes_controller.index);
